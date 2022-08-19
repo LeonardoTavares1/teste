cc378864a35efd7e8ed77e64aa04984f5cb786b0
unknown <gel.giacomini@gmail.com>
2022-08-19 00:38:36 -0300

    Tela de login e registro com verificação funcional

--- src/pages/entrar/index.jsx
+++ src/pages/entrar/index.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import { Button, Col, Form, FormGroup, Input, Label } from 'reactstrap';
 import './index.css'
 import Axios  from 'axios'
+import * as yup from 'yup'
 import { useNavigate } from 'react-router-dom';
 
 /* Essa página é quase a mesma coisa da pagina "Registro". Só explicarei sobre o axios aqui. 
 
-por hoje é só, darei prosseguimento outro dia.*/
+por hoje é só, darei prosseguimento outro dia.
+
+o outro dia chegou*/
 
 export function Login(){
+
+    /* o state aqui é reaproveitado da tela registro */
 
     const[values, setValues] = useState();
     
@@ -19,16 +24,34 @@ export function Login(){
         }))
     };
 
+
+    /* Const navigate serve para navegar entre as páginas através de funções */
     const navigate = useNavigate()
+
+
 
     const HandleClickButton = () =>{
 
-        Axios.get()
 
-        if (values.email == 'asda' && values.senha == '123'){
-            navigate('/generos', {replace: true})
-        }
-        else alert('usuario inexistente')
+        /* o axios está enviando as variaveis para o backend, lá dentro do backend ele é conferido e é dado return nas variaveis
+        
+        o return dele é guardado em "response" em forma de array. Nesse caso ele está na posição"[0]", que é o mesmo que a primeira posição
+        
+        apos ser conferido no "if" o navigate muda a página para a pagina conteudo.
+        "Replace:true" serve para o usuário entrar na pagina e não poder retornar no histórico*/
+
+        Axios.post("http://localhost:3001/login", {
+            email: values.email,
+            senha: values.senha
+        }).then((response) =>{
+    
+            if (values.email == response.data[0].email && values.senha ==  response.data[0].senha){
+                navigate('/generos', {replace: true})
+            }
+        })
+        
+            
+        
     }
 
     return(
@@ -67,6 +90,7 @@ export function Login(){
                     color="light"
                     size=""
                     onClick={() => HandleClickButton()}
+                    type="button"
                 >Enviar
                 </Button>
             </Form>
