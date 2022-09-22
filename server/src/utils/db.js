1b5df81d757fd14d153926108e641d3c77ae7bb1
--global <--global>
2022-09-22 10:21:50 -0300

    Adição de txt para facilitar a troca do banco dentro do colégio

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -1,13 +1,15 @@
 /* Como foi alterado o banco para SQL server, aviso que deve ser alterado as informações daqui para cada
 local de uso. E o sql server precisa ser configurado para o funcionamento em sua máquina. 
 Recomendo o link de um video para a configuração do servidor: https://youtu.be/fvpeb-MFtgQ */
-//aqui ficam as informações de conexão do banco de dados
+/*aqui ficam as informações de conexão do banco de dados
+
+OBSERVE SE O BANCO ESTÁ COMO O DA ETEC, SE NÃO DARÁ ERRO*/
 
 export const db = {
-    server: 'DESKTOP-7C99J2K',
+    server: '10.68.102.4',
     port: 1433,
     user:'sa',
-    password:'0192',
+    password:'Admin2018@',
     database:'LiberT',
     options:{
         enableArithAbort: true,
